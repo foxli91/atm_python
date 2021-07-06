c2c0b3cc3b507f9d2f5a02e417df25b22eed4728
lsdark2_pb <lsdark_cl@foxmail.com>
2021-07-06 16:36:04 +0800

    马上面向对象了

--- conf/settings.py
+++ conf/settings.py
@@ -2,3 +2,20 @@
 配置文件
 '''
 # 这里需要说明一下 台式机上传
+
+tus = tuple(['1', '2'])
+print(type(tus))
+print(tus[0])
+for i in tus:
+    print(i)
+abs = {
+    'name': '张三',
+    'age': 25,
+    'address': [1, 2, 3, 4]
+}
+
+for k, v in abs.items():
+    print(k, v)
+    print(type(v))
+    if type(v) == list:
+        print('sssss')
