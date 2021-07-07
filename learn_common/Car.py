0f811c69d0f52d44bf967b6df15fb81bada53a64
lsdark2_pb <lsdark_cl@foxmail.com>
2021-07-07 18:29:31 +0800

    修改对象的导入

--- learn_common/Car.py
+++ learn_common/Car.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from Dog import Dog
 
 class Car:
     color = ''
@@ -8,15 +9,20 @@ class Car:
     brand = ''
     owner = 'Jimmy'
 
-    def __init__(self, color, price, car_type, brand):
+    def __init__(self, color, price, car_type, brand, dog: Dog):
         self.color = color
         self.price = price
         self.car_type = car_type
         self.brand = brand
+        self.pet = dog
 
     def say_hello(self):
         print('say hello 啊')
         print(f'self-owner {self.owner}')
+
+    def set_dog(self, dog: Dog):
+        self.pet = dog
+        self.pet.bark()
 
     def __setattr__(self, name: str, value: Any) -> None:
         super().__setattr__(name, value)
@@ -32,14 +38,17 @@ class Car:
     #     return f'color={self.color},price={self.price},car_type={self.car_type},brand={self.brand}'
 
 
-bench = Car('黑色', 78904.6, 'SUV', '奔驰')
+dog = Dog('就是', 1)
+bench = Car('黑色', 78904.6, 'SUV', '奔驰', dog)
+#
+# bench.set_dog(dog)
 print(bench)
 print(bench.__str__())
 # bench.age = 15
 # print(bench.age)
 # print(bench.ages)
-bmw = Car('红色', 18904.6, '轿车', '宝马')
-audio = Car('白色', 36784.6, '轿车', '奥迪')
+bmw = Car('红色', 18904.6, '轿车', '宝马', dog)
+audio = Car('白色', 36784.6, '轿车', '奥迪', dog)
 
 # print(id(Car.say_hello))
 print(bench.say_hello)
---
+++ learn_common/Dog.py
@@ -0,0 +1,11 @@
+class Dog:
+    name = ''
+    age = 1
+
+    def __init__(self, name, age) -> None:
+        super().__init__()
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(f'我是{self.name},我在狂吠')
