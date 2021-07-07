bb4694a6d30417aeb4db3934217180a819863bae
lsdark2_pb <lsdark_cl@foxmail.com>
2021-07-07 09:47:06 +0800

    面向对象属性查找

--- learn_common/Car.py
+++ learn_common/Car.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Car:
     color = ''
     price = 0.0
@@ -9,11 +12,24 @@ class Car:
         self.price = price
         self.car_type = car_type
         self.brand = brand
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        super().__setattr__(name, value)
+
+    def __str__(self) -> str:
+        return super().__str__() + '' + f'color={self.color},price={self.price},car_type={self.car_type},brand={self.brand}'
+
     # python 中没有重载这一说具体原因看笔记
     # def __int__(self):
     #     print('hello')
     #     print(self)
+    # def to_string(self):
+    #     return f'color={self.color},price={self.price},car_type={self.car_type},brand={self.brand}'
 
 
-bench = Car()
+bench = Car('黑色', 78904.6, 'SUV', '奔驰')
 print(bench)
+print(bench.__str__())
+bench.age = 15
+print(bench.age)
+print(bench.ages)
