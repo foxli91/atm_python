0b574ad1a42ab3f8bde64d77c0e44d45327929a6
is-pb <lsdark_cl@foxmail.com>
2021-07-07 22:52:34 +0800

    property的使用2

--- learn_common/man.py
+++ learn_common/man.py
@@ -5,13 +5,28 @@ class Man:
         self.height = height
         self.sex = sex
         self.weight = weight
+        self.__wife = ''
 
     @property
     def bmi(self):
         return self.weight / (self.height ** 2)
+
+    @property
+    def wife(self):
+        return self.__wife
+
+    @wife.setter
+    def wife(self, value):
+        self.__wife = value
+
+    @wife.deleter
+    def wife(self):
+        del self.__wife
 
 
 import print_common
 
 m = Man('张三', 1.75, '男', 75)
 print(m.bmi)
+m.wife = '刘美琪'
+print(m.wife)
