b93bae177f14739b1b9428fea27d184bd683f5a5
lsdark2_pb <lsdark_cl@foxmail.com>
2021-07-07 15:26:16 +0800

    修改了函数

--- learn_common/files_demo.py
+++ learn_common/files_demo.py
@@ -3,28 +3,28 @@ import time
 
 start = time.time()
 # 这个是修改文件的一个demo
-with open('C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_HOUR_QUALITY.sql', mode='rt',
+with open('C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_MICRO_HOUR_QUALITY.sql', mode='rt',
           encoding='utf-8') as f, \
-        open('C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_HOUR_QUALITY_BAK1.sql', mode='wt',
+        open('C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_MICRO_HOUR_QUALITY_BAK1.sql', mode='wt',
              encoding='utf-8') as f1, open(
-    'C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_HOUR_QUALITY_BAK2.sql', mode='wt',
+    'C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_MICRO_HOUR_QUALITY_BAK2.sql', mode='wt',
     encoding='utf-8') as f2, open(
-    'C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_HOUR_QUALITY_BAK3.sql', mode='wt',
+    'C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_MICRO_HOUR_QUALITY_BAK3.sql', mode='wt',
     encoding='utf-8') as f3, open(
-    'C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_HOUR_QUALITY_BAK4.sql', mode='wt',
+    'C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_MICRO_HOUR_QUALITY_BAK4.sql', mode='wt',
     encoding='utf-8') as f4, open(
-    'C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_HOUR_QUALITY_BAK5.sql', mode='wt',
+    'C:\\Users\\Administrator.SC-201712251822\\Desktop\\大气资料\\T_AIR_SITE_MICRO_HOUR_QUALITY_BAK5.sql', mode='wt',
     encoding='utf-8') as f5:
     c = 0
     for line in f:
 
-        if c <= 1000000:
+        if c <= 192439:
             f1.write(line.replace('\"DB_GSODS_AREV_BASE\".', ''))
-        elif c <= 2000000:
+        elif c <= 292439:
             f2.write(line.replace('\"DB_GSODS_AREV_BASE\".', ''))
-        elif c <= 3000000:
+        elif c <= 692439:
             f3.write(line.replace('\"DB_GSODS_AREV_BASE\".', ''))
-        elif c <= 4000000:
+        elif c <= 992439:
             f4.write(line.replace('\"DB_GSODS_AREV_BASE\".', ''))
         else:
             f5.write(line.replace('\"DB_GSODS_AREV_BASE\".', ''))
