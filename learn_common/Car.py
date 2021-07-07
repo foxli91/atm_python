31d12d723c59304e7629dfa231885277ea567923
lsdark2_pb <lsdark_cl@foxmail.com>
2021-07-07 14:48:33 +0800

    面向对象属性查找

--- learn_common/Car.py
+++ learn_common/Car.py
@@ -6,12 +6,17 @@ class Car:
     price = 0.0
     car_type = ''
     brand = ''
+    owner = 'Jimmy'
 
     def __init__(self, color, price, car_type, brand):
         self.color = color
         self.price = price
         self.car_type = car_type
         self.brand = brand
+
+    def say_hello(self):
+        print('say hello 啊')
+        print(f'self-owner {self.owner}')
 
     def __setattr__(self, name: str, value: Any) -> None:
         super().__setattr__(name, value)
@@ -30,6 +35,29 @@ class Car:
 bench = Car('黑色', 78904.6, 'SUV', '奔驰')
 print(bench)
 print(bench.__str__())
-bench.age = 15
-print(bench.age)
-print(bench.ages)
+# bench.age = 15
+# print(bench.age)
+# print(bench.ages)
+bmw = Car('红色', 18904.6, '轿车', '宝马')
+audio = Car('白色', 36784.6, '轿车', '奥迪')
+
+# print(id(Car.say_hello))
+print(bench.say_hello)
+print(bench)
+print(audio.say_hello)
+print(bmw.say_hello)
+print('-----------------------owner------------')
+print(Car.owner)
+print(bench.owner)
+print(audio.owner)
+print(bmw.owner)
+bench.owner = 'Lishuai'
+print('-----------------------修改某个owner------------')
+print(Car.owner)
+print(bench.owner)
+print(audio.owner)
+print(bmw.owner)
+# print(id(Car.owner))
+# print(id(bench.owner))
+# print(id(audio.owner))
+# print(id(bmw.owner))
