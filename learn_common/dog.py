5df2f0798a99716f56e637b51485b7fb093d11a9
is-pb <lsdark_cl@foxmail.com>
2021-07-08 22:44:09 +0800

    内置方法库

--- learn_common/dog.py
+++ learn_common/dog.py
@@ -7,9 +7,43 @@ class Dog:
         self.name = name
         self.age = age
 
+    def __str__(self) -> str:
+        return f'名称：{self.name},年龄：{self.age}'
+
     def bark(self):
         print(f'我是{self.name},我在狂吠')
 
 
 print(Dog.__base__)
 print(Dog.__bases__)
+dog1 = Dog('hll1', 5)
+print(isinstance(dog1, Dog))
+dog2 = Dog('ob', 2)
+dog3 = Dog('cgd', 3)
+dog4 = Dog('nds', 2)
+dog5 = Dog('liuw', 6)
+dog_list = [dog1, dog2, dog3, dog4, dog5]
+import print_common
+
+new_list = filter(lambda dg: dg.age >= 3, dog_list)
+for d in new_list:
+    print(d)
+print_common.ps()
+mx = max(dog_list, key=lambda dg: dg.age)
+print(mx)
+print_common.ps()
+sk = sorted(dog_list, key=lambda dg: dg.age)
+for s in sk:
+    print(s)
+print_common.ps()
+fz=reversed(sk)
+for s in fz:
+    print(s)
+print_common.ps()
+names = map(lambda dg: dg.name, dog_list)
+for i in names:
+    print(i)
+
+# import 'time' # 错误的用法
+ts=__import__('time')
+print(ts.time())
---
+++ learn_common/fangfa.py
@@ -0,0 +1,14 @@
+import print_common
+
+r1 = bin(11)
+print_common.ps()
+print(r1)
+r2 = oct(11)
+print(r2)
+print(hex(11))
+
+print(chr(65))
+print(ord('A'))
+
+print(divmod(105, 10))
+print(eval('1+(2+5)*6'))
