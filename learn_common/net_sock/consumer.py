91f538e6a15c65209712154067c555a672bc52c1
is-pb <lsdark_cl@foxmail.com>
2021-07-10 14:28:06 +0800

    socket 连接版本1

--- learn_common/net_sock/consumer.py
+++ learn_common/net_sock/consumer.py
@@ -1,14 +1,20 @@
 import socket
 
-ip_port = ('127.0.0.1', 9000)
-BUF_SIZE = 1024
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+i = 0
+while True:
+    ip_port = ('127.0.0.1', 9000)
+    BUF_SIZE = 1024
+    consumer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect_ex(ip_port)  # 开始按照ip连接
+    consumer.connect_ex(ip_port)  # 开始按照ip连接
+    message = f'我是服务端我开始发送消息了，is牛逼{i}'
+    print('发送的时候测试' + message)
+    consumer.send(message.encode('utf-8'))  # 发消息,说话(只能发送字节类型)
 
-s.send('我是服务端我开始发送消息了，is牛逼'.encode('utf-8'))  # 发消息,说话(只能发送字节类型)
+    feedback = consumer.recv(BUF_SIZE)  # 接收服务端发来的消息
+    print(feedback.decode('utf-8'))
 
-feedback = s.recv(BUF_SIZE)  # 接收服务端发来的消息
-print(feedback.decode('utf-8'))
-
-s.close()  # 客户端关闭
+    consumer.close()  # 客户端关闭
+    i += 1
+    if i == 100:
+        break
---
+++ learn_common/net_sock/ns.py
@@ -0,0 +1,17 @@
+from learn_common.print_common import ps
+
+i = 0
+s = ''
+j = '是多少'
+k = None
+ps()
+if not i:
+    print('这个是啥?')
+if not s:  # 'S为空的取反'
+    print('S为空的取反')
+if j:
+    print('j不为空')
+if k:
+    print('哈哈哈')
+if not k:
+    print('不撒旦发射点')
