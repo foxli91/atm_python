efd32d23520a8121f3aeab32b1d77d8de1187d0d
lsdark2_pb <lsdark_cl@foxmail.com>
2021-07-15 16:34:08 +0800

    构造方法说明2

--- learn_common/entity_demo/boy.py
+++ learn_common/entity_demo/boy.py
@@ -3,12 +3,17 @@ from learn_common.entity_demo.human import Human
 
 class Boy(Human):
 
-    def __init__(self, package: str, name: str, age: int, address: str, sex: str) -> None:
-        super().__init__(name, age, address, sex)
+    def __init__(self, package: str) -> None:
         self.package = package
 
 
+# def __init__(self, package: str, name: str, age: int, address: str, sex: str) -> None:
+#     super().__init__(name, age, address, sex)
+#     self.package = package
+
+
 if __name__ == '__main__':
-    b = Boy('迪士尼书包', '张三', 15, '成都市', '男')
+    # b = Boy('迪士尼书包', '张三', 15, '成都市', '男')
+    b = Boy('迪士尼书包')
     print(b.package)
-    print(b.sex)
+    print('sex=', b.sex)
--- learn_common/entity_demo/human.py
+++ learn_common/entity_demo/human.py
@@ -1,14 +1,19 @@
 class Human:
-    def __init__(self, name: str, age: int, address: str, sex: str) -> None:
-        self.name = name
-        self.age = age
-        self.address = address
-        self.sex = sex
+    name = 'name'
+    age = 'age'
+    address = 'address'
+    sex = ''
+
+    # def __init__(self, name: str, age: int, address: str, sex: str) -> None:
+    #     self.name = name
+    #     self.age = age
+    #     self.address = address
+    #     self.sex = sex
 
     def __str__(self) -> str:
         return super().__str__()
 
 
-if __name__ == '__main__':
-    h = Human('张三', 15, '北京市朝阳区', '男')
-    print(h.name, h.address, h.age, h.sex)
+# if __name__ == '__main__':
+# h = Human('张三', 15, '北京市朝阳区', '男')
+# print(h.name, h.address, h.age, h.sex)
