47cce6bfdee24aae9bf30c0dfd65486ee61381e1
lsdark2_pb <lsdark_cl@foxmail.com>
2021-07-15 16:49:41 +0800

    构造方法说明3

--- learn_common/entity_demo/boy.py
+++ learn_common/entity_demo/boy.py
@@ -3,13 +3,16 @@ from learn_common.entity_demo.human import Human
 
 class Boy(Human):
 
-    def __init__(self, package: str) -> None:
+    #
+    # def __init__(self, package: str) -> None:
+    #     self.package = package
+
+    def __init__(self, package: str, name: str, age: int, address: str, sex: str) -> None:
+        '''
+         这里第一行必须遵循规范调用父类的构造方法
+        '''
+        super().__init__(name, age, address, sex)
         self.package = package
-
-
-# def __init__(self, package: str, name: str, age: int, address: str, sex: str) -> None:
-#     super().__init__(name, age, address, sex)
-#     self.package = package
 
 
 if __name__ == '__main__':
--- learn_common/entity_demo/human.py
+++ learn_common/entity_demo/human.py
@@ -4,15 +4,14 @@ class Human:
     address = 'address'
     sex = ''
 
-    # def __init__(self, name: str, age: int, address: str, sex: str) -> None:
-    #     self.name = name
-    #     self.age = age
-    #     self.address = address
-    #     self.sex = sex
+    def __init__(self, name: str, age: int, address: str, sex: str) -> None:
+        self.name = name
+        self.age = age
+        self.address = address
+        self.sex = sex
 
     def __str__(self) -> str:
         return super().__str__()
-
 
 # if __name__ == '__main__':
 # h = Human('张三', 15, '北京市朝阳区', '男')
